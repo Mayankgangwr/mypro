e466f0b0d4a22cac37798b7a5365de7d884bbbce
Mayank Gangwar <73337152+Mayankgangwr@users.noreply.github.com>
2023-01-05 11:39:25 +0530

    Update cart.js

--- src/cart.js
+++ src/cart.js
@@ -1,101 +1,158 @@
-import React, {useState}  from 'react';
-import {useNavigate} from "react-router-dom"
-import './menu.css';
-const CartData = ({cart, deleteCart,minus,plus, show, restrodata, PlaceOrder, clientname, handleClientName}) => {
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./menu.css";
+const CartData = ({
+  cart,
+  deleteCart,
+  minus,
+  plus,
+  show,
+  restrodata,
+  PlaceOrder,
+  clientname,
+  handleClientName,
+}) => {
   const navigate = useNavigate();
-   const amount = cart.reduce((total, item) => {
-      return total + parseInt(item.qty)*parseInt(item.price);},0);
-return (
-     <>
-<section className="main">
-  <div className="container-fluid">
-  <h1 className="text-center mt-3" style={{fontFamily: 'Aclonica', textShadow: '1.2px 2px 3px #6c6767',
-    color: '#d82643'}}>MY CART</h1>
+  const amount = cart.reduce((total, item) => {
+    return total + parseInt(item.qty) * parseInt(item.price);
+  }, 0);
+  return (
+    <>
+      <section className="main">
+        <div className="container-fluid">
+          <h1
+            className="text-center mt-3"
+            style={{
+              fontFamily: "Aclonica",
+              textShadow: "1.2px 2px 3px #6c6767",
+              color: "#d82643",
+            }}
+          >
+            MY CART
+          </h1>
 
-    <hr/>
-    <div className="row">
-    {cart.map(({id,img,qty,title,mrp,price}) => (
-          <div key={id} className="col-xl-3 card-item col-lg-4 col-md-6 col-sm-6 col-xs-6 col-12 mt-3">
-               <div className="card">
-                   <div className="card-body p-2">
-                      <div className="d-flex text-black p-0">
-                         <img src={img}
-                           alt="Generic placeholder image" className="item-logo" />    
-                         <div className='row ps-3'> 
-                         <div className='col-10'>
-                                   <h6 className='text-start'>{title}</h6>
-                              </div>
-                              <div className='col-2 text-end'>
-                              <h5><i  
-                              onClick={deleteCart} 
-                              data-id={id} 
-                              className="far fa-trash-alt">
-                              </i></h5>
-                              </div>
-                              <div className='col-6 d-flex'>
-                              <button className='btn btn-primary h-75 p-qty' name={id} onClick={minus}>-</button>
-                              <span className='form-control h-75 mx-1 pt-1 text-center'>{qty}</span>
-                              <button className='btn btn-primary h-75 p-qty' name={id} onClick={plus}>+</button>
-                              </div>
-                              <div className='col-6 pt-1 text-end'>
-                                 <span className='price sale-price pe-2'>${price}</span>
-                                 <span className='price mrp'>${mrp}</span>
-                              </div>
-                         </div>
+          <hr />
+          <div className="row">
+            {cart.map(({ id, img, qty, title, mrp, price }) => (
+              <div
+                key={id}
+                className="col-xl-3 card-item col-lg-4 col-md-6 col-sm-6 col-xs-6 col-12 mt-3"
+              >
+                <div className="card">
+                  <div className="card-body p-2">
+                    <div className="d-flex text-black p-0">
+                      <img
+                        src={img}
+                        alt="Generic placeholder image"
+                        className="item-logo"
+                      />
+                      <div className="row ps-3">
+                        <div className="col-10">
+                          <h6 className="text-start">{title}</h6>
+                        </div>
+                        <div className="col-2 text-end">
+                          <h5>
+                            <i
+                              onClick={deleteCart}
+                              data-id={id}
+                              className="far fa-trash-alt"
+                            ></i>
+                          </h5>
+                        </div>
+                        <div className="col-6 d-flex">
+                          <button
+                            className="btn btn-primary h-75 p-qty"
+                            name={id}
+                            onClick={minus}
+                          >
+                            -
+                          </button>
+                          <span className="form-control h-75 mx-1 pt-1 text-center">
+                            {qty}
+                          </span>
+                          <button
+                            className="btn btn-primary h-75 p-qty"
+                            name={id}
+                            onClick={plus}
+                          >
+                            +
+                          </button>
+                        </div>
+                        <div className="col-6 pt-1 text-end">
+                          <span className="price sale-price pe-2">
+                            ${price}
+                          </span>
+                          <span className="price mrp">${mrp}</span>
+                        </div>
                       </div>
-                   </div>
-               </div>
-          </div>             
-      ))}  
-
-      <div className={`col-12 ${show} mt-3`}>
-        <div className="card mb-4">
-          <div className="card-header py-2">
-            <h5 className="mb-0">Summary</h5>
-          </div>
-          <div className="card-body px-3 pb-3 pt-1">
-            <ul className="list-group list-group-flush">
-              <li
-                className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
-                Products
-                <span>${(amount*82)/100}</span>
-              </li>
-              <li className="list-group-item d-flex justify-content-between align-items-center px-0">
-                GST(18%)
-                <span>${(amount*18)/100}</span>
-              </li>
-              <li
-                className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
-                <div>
-                  <strong>Total amount</strong>
+                    </div>
+                  </div>
                 </div>
-                <span><strong>${amount}</strong></span>
-              </li>
-            </ul>
-            <input type="text" className='form-control mb-3' placeholder='Enter Your Name' onChange={handleClientName} value={clientname}/>
-            <button onClick={()=>
-            {
-              if(clientname!=''){
-              PlaceOrder();
-              if(amount>0){
-                navigate(`/${restrodata.id}/${restrodata.tableno}/order`);
-              }
-            }else{
-              alert("Enter Your Name");
-            }
-            }
-              } className="btn btn-primary btn-lg btn-block">
-              Go to checkout
-            </button>
+              </div>
+            ))}
+
+            <div className={`col-12 ${show} mt-3`}>
+              <div className="card mb-4">
+                <div className="card-header py-2">
+                  <h5 className="mb-0">Summary</h5>
+                </div>
+                <div className="card-body px-3 pb-3 pt-1">
+                  <ul className="list-group list-group-flush">
+                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 pb-0">
+                      Products
+                      <span>${(amount * 82) / 100}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center px-0">
+                      GST(18%)
+                      <span>${(amount * 18) / 100}</span>
+                    </li>
+                    <li className="list-group-item d-flex justify-content-between align-items-center border-0 px-0 mb-3">
+                      <div>
+                        <strong>Total amount</strong>
+                      </div>
+                      <span>
+                        <strong>${amount}</strong>
+                      </span>
+                    </li>
+                  </ul>
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Enter Your Name"
+                    onChange={handleClientName}
+                    value={clientname}
+                  />
+                  <button
+                    onClick={() => {
+                      if (clientname != "") {
+                        PlaceOrder();
+                        if (amount > 0) {
+                          navigate(
+                            `/${restrodata.id}/${restrodata.tableno}/order`
+                          );
+                        }
+                      } else {
+                        alert("Enter Your Name");
+                      }
+                    }}
+                    className="btn btn-primary btn-lg btn-block"
+                  >
+                    Go to checkout
+                  </button>
+                  <p
+                    className="text-center text-danger mt-4"
+                    style={{ fontSize: "15px" }}
+                  >
+                    Please Dont Reload or Close window Brefore Complete Your
+                    Order.
+                  </p>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-
-    </div>
-   </div>
-</section>
-
-          </>
-     );
+      </section>
+    </>
+  );
 };
 export default CartData;
