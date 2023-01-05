d289a4dcd6f67e0c14b50f259c4a7a3bd35c57ca
Mayank Gangwar <73337152+Mayankgangwr@users.noreply.github.com>
2023-01-05 12:59:37 +0530

    Update menu.js

--- src/menu.js
+++ src/menu.js
@@ -54,19 +54,34 @@ const Rectro = ({ FoodCard, cart, AddCart, show, contoinue }) => {
 
   return (
     <>
+      <nav key="1" className={`navbar  navbar-expand nav-bg`}>
+        <div className="container-fluid">
+          <div className="collapse navbar-collapse">
+            <div className="text-center me-auto text-style">
+              <img
+                src="/images/logo.png"
+                height="40"
+                alt="MDB Logo"
+                loading="lazy"
+              />
+            </div>
+            <h1
+              className="me-auto mb-0"
+              style={{
+                fontFamily: "Aclonica",
+                textShadow: "1.2px 2px 3px #6c6767",
+                color: "#d82643",
+              }}
+            >
+              MENU
+            </h1>
+          </div>
+        </div>
+      </nav>
+
       <section className="main mb-5">
         <div className="container-fluid">
-          <h1
-            className="text-center mt-3"
-            style={{
-              fontFamily: "Aclonica",
-              textShadow: "1.2px 2px 3px #6c6767",
-              color: "#d82643",
-            }}
-          >
-            OUR MENU
-          </h1>
-          <div className="row">
+          <div className="row mt-3">
             <div className="col-6 my-1 mx-0 py-1">
               <select className="form-control" name="cat" onChange={Filtered}>
                 <option value="">Select Cat</option>
